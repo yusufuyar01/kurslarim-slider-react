ef55f5c7b63aa602c2675178933869e829cface7
Yusuf UYAR <uyaryusuf01@gmail.com>
2025-02-13 01:09:59 +0300

    sağ sol butonların eklenmesi

--- src/Courses.js
+++ src/Courses.js
@@ -1,18 +1,37 @@
 import Course from "./Course";
+import { useState } from "react";
+import { FaChevronLeft, FaChevronRight  } from "react-icons/fa";
 
-function Courses({courses , renoveCourse}) {
-    
+function Courses({courses }) {
+    const [index, setIndex] = useState(0)
+    const { content, title, price}  = courses[index];
+
+    const prevCourse = () => {
+        setIndex((index) => {
+            let newIndex = index - 1;
+            return checkIndex(newIndex);
+        })
+    }
+    const nextCourse = () => {
+        setIndex((index) => {
+            let newIndex = index + 1;
+            return checkIndex(newIndex);
+        })
+    }
+    const checkIndex = (index)  => {}
+
     return <div className="courses-general">
         <div>
             <h2>KURSLARIM</h2>
         </div>
-        <div className="courses">
-            {
-                courses.map((course) => {
-                    return <Course key={course.id} {...course} renoveOneCourse = {renoveCourse} />
-                    // course={course} bu propsu kullanmak yerine ...course kullandım. Böylece coursenin içindekileri props olarak gönderebildim.
-                })
-            }
+        <div className="cardDiv">
+        <button className="prevNext" onClick={prevCourse}><FaChevronLeft /></button>
+        <div className="cardTitlePrice">
+            <h2 className="cardTitle">{title}</h2>
+            <p>{content}</p>
+            <h4 className="cardPrice">{price}TL</h4>
+        </div>
+        <button className="prevNext" onClick={nextCourse}><FaChevronRight /></button>
         </div>
     </div> ;
 }
